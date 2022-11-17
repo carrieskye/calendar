eb2b1f73d550a2506a25f32ce7b975cb2862c6cd
Carolyne Peelman <peelmancarolyne@gmail.com>
2022-11-17 15:19:55 +0100

    Flat list of scripts

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-import logging
-import sys
+import argparse
 
-from skye_comlib.utils.formatter import Formatter
-from skye_comlib.utils.input import Input
 from skye_comlib.utils.logger import Logger
 
 from src.scripts.activity.larry_default_working_day import LarryDefaultWorkingDayScript
@@ -18,52 +15,45 @@ from src.scripts.media.add_to_calendar import AddToCalendar
 
 Logger.configure()
 
-SCRIPTS = {
-    "Activity": {
-        "Parse timing export": ParseTimingExportScript,
-        "Update calendar": UpdateCalendar,
-        "Parse Hayley export": ParseHayleyExportScript,
-        "Larry default working day": LarryDefaultWorkingDayScript,
-    },
-    "Media": {
-        "Add Trakt watches to calendar": AddToCalendar,
-        "Add episodes to history": AddEpisodesToHistory,
-        "Add movie to history": AddMovieToHistory,
-    },
-    "Locations": {
-        "Update event times": UpdateEventTimes,
-        "Add new location": AddLocation,
-        "Print locations": PrintLocations,
-    },
-}
+
+def run_multiple(task_dict: dict, tasks_str: str):
+    task_names = list(task_dict.keys())
+    if not tasks_str:
+        tasks_str = input(
+            "Please select tasks:" + "\n".join([f"{idx}) {task}" for idx, task in enumerate(task_names)]) + "\nTasks: "
+        )
+    tasks = []
+    for number in tasks_str.split(","):
+        if "-" in number:
+            start, end = number.split("-")
+            tasks += range(int(start), int(end) + 1)
+        else:
+            tasks.append(int(number))
+    for task in tasks:
+        script = task_dict[task_names[task]]()
+        script.run()
+
 
 if __name__ == "__main__":
-    logging.info(Formatter.title("Selecting script"))
+    parser = argparse.ArgumentParser()
 
-    categories = list(SCRIPTS.keys())
-    options = "\n".join(f"{str(index + 1).rjust(4)}) {x}" for index, x in enumerate(categories))
-    if len(sys.argv) > 1:
-        category_no = int(sys.argv[1]) - 1
-    else:
-        category_no = Input.get_int_input(f"Which category do you want to run?\n{options}\n\nCategory no. ")
-        category_no -= 1
-
-    assert category_no in range(0, len(SCRIPTS))
-    category = categories[category_no]
-    logging.info(f"Selected [bold]{category}", extra={"markup": True})
-
-    scripts = list(SCRIPTS[category].keys())
-    options = "\n".join(f"{str(index + 1).rjust(4)}) {x}" for index, x in enumerate(scripts))
-    if len(sys.argv) > 2:
-        script_no = int(sys.argv[2]) - 1
-    else:
-        script_no = Input.get_int_input(f"Which script do you want to run?\n{options}\n\nScript no. ")
-        script_no -= 1
-
-    assert script_no in range(0, len(scripts))
-    script_name = scripts[script_no]
-    logging.info(f"Selected [bold]{script_name}", extra={"markup": True})
-
-    logging.info(Formatter.title("Running script"))
-    script = SCRIPTS[category][script_name]()
-    script.run()
+    FUNCTION_MAP = {
+        "Parse timing export": ParseTimingExportScript,  # 0
+        "Update calendar": UpdateCalendar,  # 1
+        "Parse Hayley export": ParseHayleyExportScript,  # 2
+        "Larry default working day": LarryDefaultWorkingDayScript,  # 3
+        "Add Trakt watches to calendar": AddToCalendar,  # 4
+        "Add episodes to history": AddEpisodesToHistory,  # 5
+        "Add movie to history": AddMovieToHistory,  # 6
+        "Update event times": UpdateEventTimes,  # 7
+        "Add new location": AddLocation,  # 8
+        "Print locations": PrintLocations,  # 9
+    }
+    parser.add_argument("--task", "--t", choices=FUNCTION_MAP.keys(), required=False)
+    parser.add_argument("--numbers", "--n", type=str, required=False)
+    args = parser.parse_args()
+    try:
+        script = FUNCTION_MAP[args.task]
+        script.run()
+    except KeyError:
+        run_multiple(FUNCTION_MAP, args.numbers)
